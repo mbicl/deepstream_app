e93b1a53f1ef01ebff0ce285dc659cee73aa302b
mbicl <baxriddinovmaqsud@gmail.com>
2025-02-24 15:49:21 +0500

    shell file for updating and installing libraries

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# update and upgrade
+apt update
+apt upgrade
+
+# install gstreamer
+apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
+
+# install other libraries
+apt install --reinstall libavcodec58 mjpegtools
+apt install ffmpeg
