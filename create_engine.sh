2faf2558d514d613a70357c5193fe437e9c1159d
mbicl <baxriddinovmaqsud@gmail.com>
2025-02-26 12:44:33 +0500

    compacted

--- create_engine.sh
+++ create_engine.sh
@@ -1,8 +1,13 @@
-# removing old engine files
+#!/bin/bash
 
+echo "removing old *.engine files"
 rm ./peoplenet/*.engine
 
-# creating engine from .onnx model file
-trtexec --onnx=./peoplenet/resnet34_peoplenet_int8.onnx --int8 \
---calib=./peoplenet/resnet34_peoplenet_int8.txt --saveEngine=./peoplenet/resnet34_peoplenet_int8.onnx_b2_gpu0_int8.engine \
---minShapes="input_1:0":1x3x544x960 --optShapes="input_1:0":2x3x544x960 --maxShapes="input_1:0":2x3x544x960
+echo "building engine"
+trtexec \
+    --onnx=./peoplenet/resnet34_peoplenet_int8.onnx --int8 \
+    --calib=./peoplenet/resnet34_peoplenet_int8.txt \
+    --saveEngine=./peoplenet/resnet34_peoplenet_int8.onnx_b2_gpu0_int8.engine \
+    --minShapes="input_1:0":1x3x544x960 \
+    --optShapes="input_1:0":2x3x544x960 \
+    --maxShapes="input_1:0":2x3x544x960
