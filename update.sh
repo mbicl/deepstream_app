54c5ea0135f2a30e907757e1b2529cde395a683e
mbicl <baxriddinovmaqsud@gmail.com>
2025-02-26 12:37:51 +0500

    compacted

--- update.sh
+++ update.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # update and upgrade
-apt update
-apt upgrade
+apt update && apt upgrade -y
 
 # install gstreamer
-apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
+apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
 
 # install other libraries
-apt install --reinstall libavcodec58 mjpegtools
-apt install ffmpeg
+apt install -y libavcodec58 mjpegtools ffmpeg
